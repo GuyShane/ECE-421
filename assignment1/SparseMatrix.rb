773d9786df765309ae90bd2e9323af88b2806467
msabbasi <msabbasi@ualberta.ca>
2014-10-09 16:17:34 -0600

    Added descriptions

--- assignment1/SparseMatrix.rb
+++ assignment1/SparseMatrix.rb
@@ -63,77 +63,78 @@ class SparseMatrix
 =end
 
   def transpose()
-  	
-    result=SparseMatrix.new(self.rows,self.cols,Hash[self.matrix.map{|k,v| [k.reverse,v]}])
+  	#Returns the transpose of the matrix
+    SparseMatrix.new(self.rows,self.cols,Hash[self.matrix.map{|k,v| [k.reverse,v]}])
   end
 
   def determinant()
-
+    #Calculates and returns the determinant of the matrix
     (1..cols).map{|i| (i == cols && i == 2 ? 0 : (self.cols == 2 ? self.matrix[[1,1]].to_f*self.matrix[[2,2]].to_f-self.matrix[[1,2]].to_f*self.matrix[[2,1]].to_f : (-1)**(i+1)*self.matrix[[i,1]].to_f*SparseMatrix.new(self.cols-1,self.rows-1,Hash[Hash[self.matrix.reject{|k,v| k[1] == 1 || k[0] == i}].map{|k,v| (k[0] < i ? [[k[0],k[1]-1],v] : [[k[0]-1,k[1]-1],v])}]).determinant))}.reduce(:+)
   end
 
   def inverse()
-
+    #Returns the inverse matrix
     self.cofactorMatrix.transpose.times(1/self.determinant)
   end
   
   def cofactorMatrix()
-  	
+  	#Returns the cofactor matrix
     SparseMatrix.new(self.cols,self.rows,Hash[self.matrix.map{|k,v| [k,self.cofactor(k[0],k[1])]}])
   end
   
   def cofactor(i,j)
-  	
+  	#Calculates and returns the cofactor of an entry in the matrix
     (-1)**(i+j)*SparseMatrix.new(self.cols-1,self.rows-1,Hash[Hash[self.matrix.reject{|x,y| x[0] == i || x[1] == j}].map{|x,y| (x[0] < i ? (x[1] < j ? [[x[0],x[1]],y] : [[x[0],x[1]-1],y]) : (x[1] < j ? [[x[0]-1,x[1]],y] : [[x[0]-1,x[1]-1],y] ))}]).determinant
   end
 
   def minor(ri,rf,ci,cf)
-
-    result=SparseMatrix.new(cf-ci+1, rf-ri+1, Hash[Hash[self.matrix.reject{|k,v| k[1] < ri || k[1] > rf || k[0] < ci || k[0] > cf}].map{|k,v| [[k[0]-ci+1,k[1]-ri+1],v]}])
+    #Returns a submatrix of the matrix given ranges of rows and columns using ri (first row), rf (last row), ci (first column), and cf (last column).
+    SparseMatrix.new(cf-ci+1, rf-ri+1, Hash[Hash[self.matrix.reject{|k,v| k[1] < ri || k[1] > rf || k[0] < ci || k[0] > cf}].map{|k,v| [[k[0]-ci+1,k[1]-ri+1],v]}])
   end
 
   def real?()
-
-    result=self.matrix.values.all?(&:real?)
+    #Checks if all the entries in a matrix are real numbers. Returns a boolean value.
+    self.matrix.values.all?(&:real?)
   end
 
   def regular?()
-  	
-    result=!self.singular?
+  	#Checks if a matrix is regular(invertible). Returns a boolean values.
+    !self.singular?
   end
 
   def round(digits=0)
-  	
-    result=SparseMatrix.new(self.cols,self.rows,Hash[self.matrix.map{|k,v| [k,v.round(digits)]}])
+  	#Rounds all the entries in the matrix to specified number of digits. Returns a new matrix.
+    SparseMatrix.new(self.cols,self.rows,Hash[self.matrix.map{|k,v| [k,v.round(digits)]}])
   end
 
   def singular?()
-    result=self.determinant == 0
+    #Checks if a matrix is singular. Returns a boolean value.
+    self.determinant == 0
   end
 
   def trace()
-
-    k=(1..rows).map{|i| self.matrix[[i,i]].to_f}.inject(0, &:+)
+    #Returns the sum of the entries in the main diagonal
+    (1..rows).map{|i| self.matrix[[i,i]].to_f}.inject(0, &:+)
   end
 
   def square?()
-  	
-    result=(self.rows == self.cols)
+  	#Checks of the number fo rows are equal to number of columns. Returns a boolean value.
+    (self.rows == self.cols)
   end
 
   def symmetric?()
-  	
+  	#Checks if the matrix is symmetric. Returns a boolean value.
     self.minus(self.transpose).zero?  
   end
 
   def zero?()
-  
+    #Checks if all the entries in the matrix are zero. Returns a boolean value.
     result=self.matrix.values.all?{|v| v == 0}
   end
 
   def SparseMatrix.identity(size)
-  	
-    result=SparseMatrix.new(size, size, Hash[(1..size).map{|i| [[i,i],1]}])
+  	#Returns an identity matrix with dimensions size x size
+    SparseMatrix.new(size, size, Hash[(1..size).map{|i| [[i,i],1]}])
   end
 
   def plus(m)
