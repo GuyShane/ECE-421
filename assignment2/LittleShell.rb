5fe245fd1ecfd5f806ce31fabf2d8dec26730cfa
GuyShane <brasss@hotmail.com>
2014-10-23 13:41:22 -0600

    Started checking the inputs

--- assignment2/LittleShell.rb
+++ assignment2/LittleShell.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 require 'shellwords'
+require 'getoptlong'
 require './DelayPrint'
+
+all_commands=['pwd','mkdir','touch','mv','ls','rm','rmdir','cat','echo','cp','cd','delay_print','FileWatchCreation','FileWatchAlter','FileWatchDestroy']
+flags={'ls'=>['--all','-a','-l'],'rm'=>['--recursive','-r','--force','f']}
 
 prompt="LittleShell-$: "
 
@@ -18,11 +22,38 @@ if ARGV.empty?
   loop do
     $stdout.print prompt
     user_in=input.gets.strip
+    good_command=true
     command,*args=Shellwords.shellsplit(user_in)
-    if (commands[command])
-      commands[command].call(*args)
-    else
-      execute(user_in)
+    if !all_commands.include? command
+      puts "Command #{command} not recognized"
+      good_command=false
+    end
+    args.each {|arg| ARGV << arg};
+    opts=GetoptLong.new(
+                        ['--all','-a',GetoptLong::NO_ARGUMENT],
+                        ['-l',GetoptLong::NO_ARGUMENT],
+                        ['--recursive','-r',GetoptLong::NO_ARGUMENT],
+                        ['--force','-f',GetoptLong::NO_ARGUMENT]
+                        )
+    opts.quiet=true
+    begin
+      opts.each do |opt,arg|
+        case opt
+        when '--force'
+          puts "Permission denied: cannot force command"
+          good_command=false
+        end
+      end
+    rescue Exception
+      puts "Invalid option passed to command: #{command}"
+      good_command=false
+    end
+    if good_command
+      if (commands[command])
+        commands[command].call(*args)
+      else
+        execute(user_in)
+      end
     end
   end
 else
