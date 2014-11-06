f5ecbe02457949173ea91c2f382157d1c5de8cdf
msabbasi <msabbasi@ualberta.ca>
2014-11-06 11:38:45 -0700

    Update Sorter.rb

--- assignment3/Sorter.rb
+++ assignment3/Sorter.rb
@@ -16,20 +16,36 @@ end
 
 def tmerge(a,b,c)
   if (b.length>a.length)
-    array=Thread.new{tmerge(b,a)}
+    thread1=Thread.new{tmerge(b,a,c)}
+    thread1.join
+    return thread1.value
   elsif (a.length+b.length==1)
     c << a[0]
+    return c
   elsif (a.length==1)
     if a[0]<=b[0]
       c[0]=a[0]
       c[1]=b[0]
+      return c
     else
       c[0]=b[0]
       c[1]=a[0]
+      return c
     end
   else
-    
+    j=binsearch(a[a.length/2],b)
+    thread2=Thread.new{tmerge(a.take(a.length/2),b.take(j),c)}
+    thread3=Thread.new{tmerge(a.drop(a.length/2),b.drop(j),c)}
+    thread2.join
+    thread3.join
+    return thread2.value+thread3.value
+  end
 end
+
+def binsearch(x,b)
+  
+end
+
 
 def mergesort(list)
   return list if list.size == 1
