2be0586518de57c1f2399fbf1b50658abe35e9b6
GuyShane <brasss@hotmail.com>
2014-11-19 19:54:10 -0700

    models and things

---
+++ assignment4/BoardController.rb
@@ -0,0 +1,11 @@
+require './Board'
+
+class BoardController
+  def initialize(board)
+    @board=board
+  end
+
+  def putPiece(column)
+    
+  end
+end
--- assignment4/GameBoard.rb
+++ assignment4/GameBoard.rb
@@ -1,3 +1,9 @@
 class GameBoard
-
+  attr_accesor :board,:rows,:columns
+  def initialize
+    @board=[]
+    10.times {@board << []}
+    @rows=10
+    @columns=10
+  end
 end
