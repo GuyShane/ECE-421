524530312b66c936ed0480c848681a6a09c085f9
GuyShane <brasss@hotmail.com>
2014-11-26 17:45:24 -0700

    communication

--- assignment5/client.rb
+++ assignment5/client.rb
@@ -1,6 +1,8 @@
 require "xmlrpc/client"
 
-server = XMLRPC::Client.new("172.28.173.246")
-sum, difference =server.call("sample.sandy", 5, 3)
+server = XMLRPC::Client.new("localhost","/",8080)
+result=server.call("sample.sandy", 5, 3)
+sum=result["sum"]
+difference=result["difference"]
 puts "sum: #{sum}, diff: #{difference}"
 
